394344c79e6a31a1dd03bf13a334ce49cb0a09a5
Ehmad <justehmadsaeed@gmail.com>
2021-09-08 12:28:53 +0500

    📦 NEW: Template colors and Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,35 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	mode: 'jit',
+	purge: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+    extend: {
+      colors: {
+        'template-purple':'#5267DF',
+        'template-red':'#FA5959',
+        'template-blue':'#242A45',
+        'template-grey':'#9194A2',
+        'template-white':'#f7f7f7',
+      }
+    },
+		fontFamily: {
+			Poppins: ['Poppins, sans-serif'],
+		},
+		container: {
+			center: true,
+			padding: '1rem',
+			screens: {
+				lg: '1124px',
+				xl: '1124px',
+				'2xl': '1124px',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
