67520f1d0398d54c3c67ec043cdf16c10588e1bc
Ehmad Saeed <justehmadsaeed@gmail.com>
2021-09-29 20:06:31 +0500

    👌 IMPROVE: Add Meta data for SEO

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,17 @@ export default function Home() {
 	return (
 		<>
 			<Head>
+				<meta charSet='utf-8' />
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1.0'
+				/>
 				<title>Landing Page Template</title>
 				<link rel='icon' href='/favicon.ico' />
+				<meta
+					name='description'
+					content='A Next.js company landing page template'
+				/>
 			</Head>
 			<div className='font-Poppins'>
 				{/* Header */}
